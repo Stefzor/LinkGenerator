75e1f603d8649296fb8d2b0f72fecc5a69665ca2
Stefzor <cloud_2489@yahoo.com>
2018-10-28 17:45:22 +0200

    refactored implementation to use regex

--- Scripts/app.js
+++ Scripts/app.js
@@ -18,8 +18,18 @@ function generateFlightLink(departureAirport, destinationAirport, destination, d
     return `https://www.momondo.ro/in?a=travelator&url=/flight-search/${departureAirport}-${destinationAirport}/${date}?sort=price_a&encoder=27_1&enc_pid=deeplinks&enc_eid=0&enc_lid=${destination}&enc_cid=article&utm_source=travelator&utm_medium=affiliate&utm_term=rev&utm_campaign=deeplinks&utm_content=${destination}`;
 }
 
-function generateLodgingsLink(affiliateTag, destination, date) {
-    return `https://www.momondo.ro/in?a=travelator&url=/hotels/${affiliateTag}/${date}/2adults?sort=price_a&encoder=27_1&enc_pid=deeplinks&enc_eid=0&enc_lid=${destination}&enc_cid=article&utm_source=travelator&utm_medium=affiliate&utm_term=rev&utm_campaign=deeplinks&utm_content=${destination}`;
+function generateLodgingsLink(hotel, destination, date) {
+    return `https://www.momondo.ro/in?a=travelator&url=/hotels/${hotel}/${date}/2adults?sort=price_a&encoder=27_1&enc_pid=deeplinks&enc_eid=0&enc_lid=${destination}&enc_cid=article&utm_source=travelator&utm_medium=affiliate&utm_term=rev&utm_campaign=deeplinks&utm_content=${destination}`;
+}
+
+function generateLink(mode, hotel, departureAirport, destinationAirport, destination, date) {
+    if (mode === 'flight') {
+        return generateFlightLink(departureAirport, destinationAirport, destination, date);
+    } else if (mode === 'lodgings') {
+        return generateLodgingsLink(hotel, destination, date);
+    } else {
+        console.log('Invalid Mode. Something went wrong: ' + mode);
+    }
 }
 
 function convertMonth(month) {
@@ -51,27 +61,77 @@ function convertMonth(month) {
     }
 }
 
-function convertToDesiredDateFormat(date) {
+var supportedDateFormatsRegex = {
+    format1: new RegExp('^[0-3]?\\d{1}[\\s](ian|feb|mar|apr|mai|iun|iul|aug|sep|nov|dec)[\\s]\\d{4}$'), //DD MMM YYYY
+    format2: new RegExp('^[0-3]?\\d{1}[-][0-3]?\\d{1}[\\s](ian|feb|mar|apr|mai|iun|iul|aug|sep|nov|dec)[\\s]\\d{4}$'), //DD-DD MMM YYYY
+    format3: new RegExp('^[0-3]?\\d{1}[\\s](ian|feb|mar|apr|mai|iun|iul|aug|sep|nov|dec)[\\s][-][\\s][0-3]?\\d{1}[\\s](ian|feb|mar|apr|mai|iun|iul|aug|sep|nov|dec)[\\s]\\d{4}$'), //DD MM - DD MM YYYY
+    format4: new RegExp('^[0-3]?\\d{1}[\\s](ian|feb|mar|apr|mai|iun|iul|aug|sep|nov|dec)[\\s]\\d{4}[\\s][-][\\s][0-3]?\\d{1}[\\s](ian|feb|mar|apr|mai|iun|iul|aug|sep|nov|dec)[\\s]\\d{4}$') //DD MM YYYY - DD MM YYYY
+}
+
+var convertedDateIntervalRegex = new RegExp('^\\d{4}[-][0-1][0-3][-][0-3]\\d{1}[/]\\d{4}[-][0-1][0-3][-][0-3]\\d{1}$');
+var convertedDateRegex = new RegExp('^\\d{4}[-][0-1][0-3][-][0-3]\\d{1}$');
+
+function convertFromRegex1(date) {
     var days = date.split(' ')[0];
     var month = convertMonth(date.split(' ')[1]);
     var year = date.split(' ')[2];
 
-    if (days.length > 2) {
-        var day1 = days.split('-')[0];
-        var day2 = days.split('-')[1];
-        if (day1.length < 2) {
-            day1 = '0' + day1;
-        }
-        if (day2.length < 2) {
-            day2 = '0' + day2;
-        }
-        return year + '-' + month + '-' + day1 + '/' +
-            year + '-' + month + '-' + day2;
-    } else if (days.length === 2) {
-        return year + '-' + month + '-' + days;
-    } else {
-        return year + '-' + month + '-0' + days;
+    if (days.length < 2) {
+        days = '0' + days;
     }
+
+    return `${year}-${month}-${days}`;
+}
+
+function convertFromRegex2(date) {
+    var day1 = date.split('-')[0];
+    var day2 = new RegExp('[-][0-3]?\\d{1}').exec(date)[0].slice(1);
+    var month = new RegExp('(ian|feb|mar|apr|mai|iun|iul|aug|sep|nov|dec)').exec(date)[0];
+    var year = new RegExp('\\d{4}').exec(date)[0];
+
+    var date1 = `${day1} ${month} ${year}`;
+    var date2 = `${day2} ${month} ${year}`;
+
+    return convertFromRegex1(date1) + '/' + convertFromRegex1(date2);
+}
+
+function convertFromRegex3(date) {
+    var date1 = date.split(' - ')[0] + ' ' + new RegExp('\\d{4}').exec(date)[0];
+    var date2 = date.split(' - ')[1];
+
+    return convertFromRegex1(date1) + '/' + convertFromRegex1(date2);
+}
+
+function convertFromRegex4(date) {
+    var date1 = date.split(' - ')[0];
+    var date2 = date.split(' - ')[1];
+
+    return convertFromRegex1(date1) + '/' + convertFromRegex1(date2);
+}
+
+function convertDate(date) {
+    var convertedDate;
+    if (supportedDateFormatsRegex.format1.test(date)) {
+        convertedDate = convertFromRegex1(date);
+    }
+
+    if (supportedDateFormatsRegex.format2.test(date)) {
+        convertedDate = convertFromRegex2(date);
+    }
+
+    if (supportedDateFormatsRegex.format3.test(date)) {
+        convertedDate = convertFromRegex3(date);
+    }
+
+    if (supportedDateFormatsRegex.format4.test(date)) {
+        convertedDate = convertFromRegex4(date);
+    }
+    
+    if (!convertedDateRegex.test(convertedDate) && !convertedDateIntervalRegex.test(convertedDate)) {
+        console.log('Date is in the wrong format: ', convertedDate);
+    }
+
+    return convertedDate;
 }
 
 function convertDatesToLinks() {
@@ -84,29 +144,9 @@ function convertDatesToLinks() {
     var dates = $('#dates').val().split(/\n/);
 
     var links = dates.map(function (date) {
-        if (date.trim().length <= 14) {
-            var convertedDate = convertToDesiredDateFormat(date);
-
-            if (mode === 'flight') {
-                return generateFlightLink(departureAirport, destinationAirport, destination, convertedDate);
-            } else if (mode === 'lodgings') {
-                return generateLodgingsLink(hotel, destination, convertedDate);
-            }
-
-        } else {
-            var startDate = date.split(' - ')[0];
-            var endDate = date.split(' - ')[1];
-            if (startDate.length < 11) {
-                startDate += ' ' + endDate.split(' ')[2];
-            }
-            var convertedDate = convertToDesiredDateFormat(startDate) + '/' + convertToDesiredDateFormat(endDate);
-
-            if (mode === 'flight') {
-                return generateFlightLink(departureAirport, destinationAirport, destination, convertedDate);
-            } else if (mode === 'lodgings') {
-                return generateLodgingsLink(hotel, destination, convertedDate);
-            }
-        }
+        var convertedDate = convertDate(date);
+        
+        return generateLink(mode, hotel, departureAirport, destinationAirport, destination, convertedDate);
     });
 
     $('#links').val(links.join('\n'));
