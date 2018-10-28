46bc460cc9ba81606afad825ac34bd66a11a74fa
Stefzor <cloud_2489@yahoo.com>
2018-10-28 14:31:13 +0200

    Added project files

---
+++ Scripts/app.js
@@ -0,0 +1,85 @@
+function generateLink(departureAirport, destinationAirport, destination, date) {
+    return 'https://www.momondo.ro/in?a=travelator&url=/flight-search/'
+        + departureAirport + '-' + destinationAirport + '/' + date
+        + '?sort=price_a&encoder=27_1&enc_pid=deeplinks&enc_eid=0&enc_lid=' + destination
+        + '&enc_cid=article&utm_source=travelator&utm_medium=affiliate&utm_term=rev&utm_campaign=deeplinks&utm_content=' + destination;
+}
+
+function convertMonth(month) {
+    switch (month.toLowerCase()) {
+        case 'ian':
+            return '01';
+        case 'feb':
+            return '02';
+        case 'mar':
+            return '03';
+        case 'apr':
+            return '04';
+        case 'mai':
+            return '05';
+        case 'iun':
+            return '06';
+        case 'iul':
+            return '07';
+        case 'aug':
+            return '08';
+        case 'sep':
+            return '09';
+        case 'oct':
+            return '10';
+        case 'nov':
+            return '11';
+        case 'dec':
+            return '12';
+    }
+}
+
+function convertToDesiredDateFormat(date) {
+    var days = date.split(' ')[0];
+    var month = convertMonth(date.split(' ')[1]);
+    var year = date.split(' ')[2];
+
+    if (days.length > 2) {
+        var day1 = days.split('-')[0];
+        var day2 = days.split('-')[1];
+        if (day1.length < 2) {
+            day1 = '0' + day1;
+        }
+        if (day2.length < 2) {
+            day2 = '0' + day2;
+        }
+        return year + '-' + month + '-' + day1 + '/' +
+            year + '-' + month + '-' + day2;
+    } else if (days.length === 2) {
+        return year + '-' + month + '-' + days;
+    } else {
+        return year + '-' + month + '-0' + days;
+    }
+}
+
+function convertDatesToLinks() {
+    var destination = $('#destination').val();
+    var departureAirport = $('#departureAirport').val();
+    var destinationAirport = $('#destinationAirport').val();
+    var dates = $('#dates').val().split(/\n/);
+
+    var links = dates.map(function (date) {
+        if (date.trim().length <= 14) {
+            var convertedDate = convertToDesiredDateFormat(date);
+
+            return generateLink(departureAirport, destinationAirport, destination, convertedDate);
+        } else {
+            var startDate = date.split(' - ')[0];
+            var endDate = date.split(' - ')[1];
+            if (startDate.length < 11) {
+                startDate += ' ' + endDate.split(' ')[2];
+            }
+            var convertedDate = convertToDesiredDateFormat(startDate) + '/' + convertToDesiredDateFormat(endDate);
+
+            return generateLink(departureAirport, destinationAirport, destination, convertedDate);
+        }
+    });
+
+    $('#links').val(links.join('\n'));
+    $('#linksContainer').show();
+}
